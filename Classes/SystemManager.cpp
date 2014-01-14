4a125d38df199f3260e99340098a7d4a8c9d02e1
Wang-YiChun <wang-yichun@meyougames.com>
2014-01-15 01:46:43 +0800

    game_data.sqlite can read & write
    but not the real data

--- Classes/SystemManager.cpp
+++ Classes/SystemManager.cpp
@@ -1,4 +1,4 @@
-#include "SystemManager.h"
+﻿#include "SystemManager.h"
 #include "sqlite_header.h"
 #include "YFile.h"
 
@@ -32,6 +32,12 @@ bool SystemManager::init() {
 		CCLOG("game_data.sqlite has been copied.");
 	}
 
+	m_db_full_path = writable_path + "game_data.sqlite";
+
+	//TStage t_stage;
+	//insert_stage(t_stage);
+	select_stage();
+
 	return true;
 }
 
@@ -41,4 +47,53 @@ SystemManager * SystemManager::GetInstance() {
 		CCAssert(s_SystemManager -> init(), "SystemManager::init failed");
 	}
 	return s_SystemManager;
+}
+
+void SystemManager::insert_stage(const TStage& t_stage) {
+	try {
+		Kompex::SQLiteDatabase *pDatabase = new Kompex::SQLiteDatabase(m_db_full_path.c_str(), SQLITE_OPEN_READWRITE, 0);
+		Kompex::SQLiteStatement *pStmt = new Kompex::SQLiteStatement(pDatabase);
+
+		pStmt -> Sql("INSERT INTO stage (cell_height, cell_width, map_grid_height, map_grid_width, stage_id, stage_name) VALUES(@cell_height, @cell_width, @map_grid_height, @map_grid_width, @stage_id, @stage_name)");
+
+		pStmt -> BindInt(1, 40);
+		pStmt -> BindInt(2, 40);
+		pStmt -> BindInt(3, 20);
+		pStmt -> BindInt(4, 32);
+		pStmt -> BindInt(5, 102);
+		pStmt -> BindString(6, "中文关卡名");
+
+		pStmt -> ExecuteAndFree();
+
+		delete pStmt;
+		delete pDatabase;
+
+	} catch(Kompex::SQLiteException &exception) {
+		//std::cerr << "\nException Occured" << std::endl;
+		CCLOGERROR("\nException Occured: \n%s", exception.GetString().c_str());
+		exception.Show();
+	}
+}
+
+void SystemManager::select_stage() {
+	try {
+		Kompex::SQLiteDatabase *pDatabase = new Kompex::SQLiteDatabase(m_db_full_path.c_str(), SQLITE_OPEN_READWRITE, 0);
+		Kompex::SQLiteStatement *pStmt = new Kompex::SQLiteStatement(pDatabase);
+
+		pStmt -> Sql("SELECT * FROM stage");
+		
+		while (pStmt -> FetchRow()) {
+			int stage_id = pStmt -> GetColumnInt("stage_id");
+			string stage_name = pStmt -> GetColumnString("stage_name");
+			CCLOG("(%d,%s)", stage_id, stage_name.c_str());
+		}
+
+		delete pStmt;
+		delete pDatabase;
+
+	} catch(Kompex::SQLiteException &exception) {
+		//std::cerr << "\nException Occured" << std::endl;
+		CCLOGERROR("\nException Occured: \n%s", exception.GetString().c_str());
+		exception.Show();
+	}
 }
--- Classes/SystemManager.h
+++ Classes/SystemManager.h
@@ -3,6 +3,15 @@
 #include "MapCell.h"
 USING_NS_CC;
 
+struct TStage {
+	int cell_height;
+	int cell_width;
+	int map_grid_height;
+	int map_grid_width;
+	int stage_id;
+	string stage_name;
+};
+
 class SystemManager {
 private:
 	SystemManager();
@@ -11,6 +20,12 @@ private:
 	bool init();
 public:
 	static SystemManager * GetInstance();
+
+	// db full path.
+	string m_db_full_path;
+	// db action.
+	void insert_stage(const TStage& t_stage);
+	void select_stage();
 
 	// MapCreate active vars.
 	enumMapCellCode m_chosedCellCode;
